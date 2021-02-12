0fd54e6dcec572b8cd0dfc45849442e2b259f8c2
Andrew Wolfe <wolfe_nv@msn.com>
2021-02-11 21:52:26 -0800

    Initial V1 checking

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM bkimminich/juice-shop
+COPY ./loraxfiles/lorax.yml /juice-shop/config/lorax.yml
---
+++ Lorax-start.sh
@@ -0,0 +1,3 @@
+
+docker run -d -e "NODE_ENV=lorax"  -p 3000:3000 --name Lorax lorax 
+docker container ps
